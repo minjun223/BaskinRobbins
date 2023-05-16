d6326c6605fb477c8b9b466f6c09d4e02651c697
김민준 <80810346+minjun223@users.noreply.github.com>
2023-05-16 18:22:33 +0900

    baskin

---
+++ app/src/main/java/com/example/baskinrobbins/compose/My_Order_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.baskinrobbins.compose
+
+import android.os.Bundle
+import com.example.baskinrobbins.R
+
+class My_Order_Activity : AppCompatActivity() {
+    fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_order)
+    }
+}
+
---
+++ app/src/main/java/com/example/baskinrobbins/compose/shopping_basket_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.baskinrobbins.compose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.baskinrobbins.R
+
+class shopping_basket_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_basket)
+    }
+}
